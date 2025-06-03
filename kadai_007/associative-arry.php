92c418b97c773b6e0abf4712c8e3b882cb5e0d14
Futoshi-Hirata <129143393+Futoshi-Hirata@users.noreply.github.com>
2025-06-03 22:13:33 +0900

    kadai_007を更新

---
+++ kadai_007/associative-arry.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $vesitable_item = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    print_r($vesitable_item);
+    ?>
+</body>
+</html>
